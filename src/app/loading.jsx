3c3fb0644cc32cfa88d23ad849e88c67a54d55f3
Farooque Reza <rezafarooque@outlook.com>
2024-05-28 11:48:41 +0530

    feat: add loading.jsx in root src folder

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-10 h-10 border-4 border-gray-200 rounded-full animate-spin border-t-sky-600" />
+    </div>
+  );
+}
